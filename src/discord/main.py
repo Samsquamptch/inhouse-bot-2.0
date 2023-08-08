b6d13f63d1f576cfb2651f47a3888e7925b75140
vincent <v.termaat@hotmail.com>
2023-08-09 01:58:10 +0200

    fixed to use credentials

--- src/discord/main.py
+++ src/discord/main.py
@@ -1,10 +1,17 @@
 import discord
+import yaml
+from yaml.loader import SafeLoader
+
+def loadToken():
+	with open('../../credentials/discord_credentials.yml') as f:
+		data = yaml.load(f, Loader=SafeLoader)
+	return data['TOKEN']
 
 async def sendMessage(message, user_message, is_private):
 	await message.channel.send("hi")
 
 def runDiscordBot():
-	token = 'MTEzODYwMjQ0NzQ2MDM3NjY2OA.G58NkE.7n3ioyav-KnOe5vb8QVGr2rZmtDIikgIlBm27s'
+	token = loadToken()
 	client = discord.Client(intents=discord.Intents.default())
 
 	@client.event
@@ -20,7 +27,8 @@ def runDiscordBot():
 		channel = str(message.channel)
 		print(f"user: {user_name} said {user_message} in channel: {channel}")
 		
-		await message.channel.send("hi")
+		if user_message == 'test':
+			await message.channel.send("hi")
 
 	client.run(token)
 
