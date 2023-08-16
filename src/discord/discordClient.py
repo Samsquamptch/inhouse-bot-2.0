4e03c67ff22c7f2d4cc317bc0b299ac6425c8bd1
Samscranch <140660422+Samscranch@users.noreply.github.com>
2023-08-16 02:41:32 +0100

    Updated variables in Modals

--- src/discord/discordClient.py
+++ src/discord/discordClient.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 import yaml
 from yaml.loader import SafeLoader
 
-
 class VerifyUserModal(discord.ui.Modal, title='Verify Registered User'):
     player_name = discord.ui.TextInput(label='User\'s name')
     verify_user = discord.ui.TextInput(label='Verify user?', max_length=1, placeholder='y/n')
@@ -29,7 +28,10 @@ class ViewUsersModal(discord.ui.Modal, title='View Users'):
     player_name = discord.ui.TextInput(label='User\'s name (use "all" for full list)')
 
     async def on_submit(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f'Showing user {self.player_name} details', ephemeral=True,
+        if self.player_name == "all":
+            await interaction.response.send_message('Showing all registered users', ephemeral=True, delete_after=10)
+        else:
+            await interaction.response.send_message(f'Showing user {self.player_name} details', ephemeral=True,
                                                 delete_after=10)
 
 
@@ -47,6 +49,10 @@ class RegisterUserModal(discord.ui.Modal, title='Player Register'):
     player_mmr = discord.ui.TextInput(label='Player MMR')
 
     async def on_submit(self, interaction: discord.Interaction):
+        #the values which need to be parsed into CSV
+        print(interaction.user.id)
+        print(self.steam_id)
+        print(self.player_mmr)
         await interaction.response.send_message('You\'ve been registered, please wait to be vouched', ephemeral=True,
                                                 delete_after=10)
 
@@ -59,12 +65,17 @@ class RoleSelectModal(discord.ui.Modal, title='Set Player Roles'):
     pos_5 = discord.ui.TextInput(label='Hard Support preference', max_length=1)
 
     async def on_submit(self, interaction: discord.Interaction):
+        print(self.pos_1)
+        print(self.pos_2)
+        print(self.pos_3)
+        print(self.pos_4)
+        print(self.pos_5)
         await interaction.response.send_message('Thank you for updating your role preferences', ephemeral=True,
                                                 delete_after=10)
 
 
-class PlayerViewModal(discord.ui.Modal, title='View Player'):
-    name = discord.ui.TextInput(label='Player Name')
+class PlayerViewModal(discord.ui.Modal, title='View Player '):
+    player_name = discord.ui.TextInput(label='Player name')
 
     async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.send_message(f'Looking for user {self.name}', ephemeral=True, delete_after=10)
@@ -75,11 +86,11 @@ class AdminChoices(discord.ui.View):
         super().__init__(timeout=None)
 
     @discord.ui.select(placeholder="Select an action here", min_values=1, max_values=1, options=[
-        discord.SelectOption(label="Verify", description="Verify a registered user"),
-        discord.SelectOption(label="Edit", description="Edit a user's details and status"),
-        discord.SelectOption(label="View", description="View all registered or a specific user"),
-        discord.SelectOption(label="Remove", description="Delete a registered user"),
-        discord.SelectOption(label="Refresh", description="Select to allow you to choose the same option")
+        discord.SelectOption(label="Verify", emoji="✅", description="Verify a registered user"),
+        discord.SelectOption(label="Edit", emoji="🖊️", description="Edit a user's details and status"),
+        discord.SelectOption(label="View", emoji="👀", description="View all registered or a specific user"),
+        discord.SelectOption(label="Remove", emoji="❌", description="Delete a registered user"),
+        discord.SelectOption(label="Refresh", emoji="♻", description="Select to allow you to refresh options")
     ]
                        )
     async def select_callback(self, interaction: discord.Interaction, select: discord.ui.Select):
@@ -101,10 +112,10 @@ class UserChoices(discord.ui.View):
         super().__init__(timeout=None)
 
     @discord.ui.select(placeholder="Select an action here", min_values=1, max_values=1, options=[
-        discord.SelectOption(label="Register", description="Register for the inhouse bot"),
-        discord.SelectOption(label="Roles", description="Update what your role preferences are"),
-        discord.SelectOption(label="Players", description="View a player based on their name or steam ID"),
-        discord.SelectOption(label="Refresh", description="Select to allow you to choose the same option")
+        discord.SelectOption(label="Register", emoji="📋", description="Register for the inhouse bot"),
+        discord.SelectOption(label="Roles", emoji="🖊️", description="Update what your role preferences are"),
+        discord.SelectOption(label="Players", emoji="👀", description="View a player based on their name or steam ID"),
+        discord.SelectOption(label="Refresh", emoji="♻", description="Select to allow you to refresh options")
     ]
                        )
     async def select_callback(self, interaction: discord.Interaction, select: discord.ui.Select):
@@ -141,6 +152,11 @@ def run_discord_bot():
     @bot.command()
     async def admin(ctx):
         await ctx.send("Admin options are below", view=AdminChoices())
+
+    @bot.command()
+    async def clear(ctx):
+        await ctx.channel.purge()
+
 
     bot.run(load_token())
 
