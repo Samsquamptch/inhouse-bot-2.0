c30b8e35b269cc9c06691567785280553fcd4630
Samscranch <140660422+Samscranch@users.noreply.github.com>
2023-08-21 20:40:06 +0100

    Preference Select now updates csv

--- src/discord/discordClient.py
+++ src/discord/discordClient.py
@@ -224,6 +224,46 @@ class RegisterButton(discord.ui.View):
         else:
             await interaction.response.send_modal(RegisterUserModal())
 
+class QueueButton(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    queue_list = []
+    queue_player_name = []
+
+    @discord.ui.button(label="Join Queue", emoji="✅",
+                       style=discord.ButtonStyle.green)
+    async def join_queue(self, interaction: discord.Interaction, button: discord.ui.Button ):
+        server = interaction.user.guild
+        role = discord.utils.get(server.roles, name="inhouse")
+        if role in interaction.user.roles:
+            if interaction.user in self.queue_list:
+                await interaction.response.send_message(content="You are already queued", ephemeral=True)
+            else:
+                self.queue_list.append(interaction.user)
+                self.queue_player_name = [user.global_name for user in self.queue_list]
+                await interaction.response.send_message(content=f'Users currently in queue:'
+                                                                f'{self.queue_player_name}')
+                await interaction.response.send_message(content="You have joined the queue", ephemeral=True)
+        else:
+            await interaction.response.send_message(content="You cannot join the queue", ephemeral=True)
+
+    @discord.ui.button(label="Leave Queue", emoji="❌",
+                       style=discord.ButtonStyle.red)
+    async def leave_queue(self, interaction: discord.Interaction, button: discord.ui.Button ):
+        server = interaction.user.guild
+        role = discord.utils.get(server.roles, name="inhouse")
+        if role in interaction.user.roles:
+            self.queue_list.remove(interaction.user)
+            self.queue_player_name.remove(interaction.user.global_name)
+            await interaction.response.send_message(content=f'Users currently in queue:'
+                                                            f'{self.queue_player_name}')
+            await interaction.response.send_message(content="You have left the queue", ephemeral=True)
+        else:
+            await interaction.response.send_message(content="You aren't in the queue", ephemeral=True)
+
+
+
 class AdminChoices(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
@@ -308,6 +348,10 @@ def run_discord_bot():
     @bot.command()
     async def admin(ctx):
         await ctx.send("Admin options are below", view=AdminChoices())
+
+    @bot.command()
+    async def queue(ctx):
+        await ctx.send("Click here to join the queue", view=QueueButton())
 
     @bot.command()
     async def clear(ctx):
