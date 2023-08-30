05891937a6a72f0a1699749602f63ffa4b925f38
Owleyy <owleyowlington@gmail.com>
2023-08-30 16:25:53 +0100

    role queue addition

--- src/discord/data_management.py
+++ src/discord/data_management.py
@@ -2,7 +2,9 @@
 
 import pandas as pd
 import csv
-
+import networkx as nx
+from networkx.algorithms import bipartite
+import scipy
 
 def update_user_data(discord_id, columns, new_data):
     user_data = pd.read_csv("../../data/users.csv")
@@ -53,11 +55,63 @@ def queue_pop(queue_ids):
     if delta1 <= delta2:
         queue = queue.drop(columns="team_dos")
         queue = queue.rename(columns={"team_uno": "team"})
+        team1 = queue.loc[queue['team'] == "Team 1"]
+        team2 = queue.loc[queue['team'] == "Team 2"]
+        pos1 = pos_graph(team1)
+        team1["pos"] = [11,12,13,14,15]
+        team1.at[pos1[11], 'pos'] = 1
+        team1.at[pos1[12], 'pos'] = 2
+        team1.at[pos1[13], 'pos'] = 3
+        team1.at[pos1[14], 'pos'] = 4
+        team1.at[pos1[15], 'pos'] = 5
+        team1 = team1.sort_values('pos')
+        print(team1)
+        pos2 = pos_graph(team2)
+        team2["pos"] = [11, 12, 13, 14, 15]
+        team2.at[pos2[11], 'pos'] = 1
+        team2.at[pos2[12], 'pos'] = 2
+        team2.at[pos2[13], 'pos'] = 3
+        team2.at[pos2[14], 'pos'] = 4
+        team2.at[pos2[15], 'pos'] = 5
+        team2 = team2.sort_values('pos')
+        print(team2)
+        queue = pd.concat([team1, team2])
         queue.to_csv("../../data/match.csv", index=False)
-        return queue.loc[queue['team'] == "Team 1", 'disc'].tolist(), queue.loc[queue['team'] == "Team 2", 'disc'].tolist()
+        return team1['disc'].tolist(), team2['disc'].tolist()
     else:
         queue = queue.drop(columns="team_uno")
         queue = queue.rename(columns={"team_dos": "team"})
+        team1 = queue.loc[queue['team'] == "Team 1"]
+        team2 = queue.loc[queue['team'] == "Team 2"]
+        pos1 = pos_graph(team1)
+        team1["pos"] = [11, 12, 13, 14, 15]
+        team1.at[pos1[11], 'pos'] = 1
+        team1.at[pos1[12], 'pos'] = 2
+        team1.at[pos1[13], 'pos'] = 3
+        team1.at[pos1[14], 'pos'] = 4
+        team1.at[pos1[15], 'pos'] = 5
+        team1 = team1.sort_values('pos')
+        print(team1)
+        pos2 = pos_graph(team2)
+        team2["pos"] = [11, 12, 13, 14, 15]
+        team2.at[pos2[11], 'pos'] = 1
+        team2.at[pos2[12], 'pos'] = 2
+        team2.at[pos2[13], 'pos'] = 3
+        team2.at[pos2[14], 'pos'] = 4
+        team2.at[pos2[15], 'pos'] = 5
+        team2 = team2.sort_values('pos')
+        print(team2)
+        queue = pd.concat([team1,team2])
         queue.to_csv("../../data/match.csv", index=False)
-        return queue.loc[queue['team'] == "Team 1", 'disc'].tolist(), queue.loc[queue['team'] == "Team 2", 'disc'].tolist()
+        return team1['disc'].tolist(), team2['disc'].tolist()
 
+
+
+def pos_graph(team):
+    G = nx.Graph()
+    for i in range(0, 5):
+        G.add_node(team.index[i])
+        for j in range(1, 6):
+            G.add_node(j + 10)
+            G.add_edge(team.index[i], j + 10, weight=team.iloc[[i], [j + 2]].values.min())
+    return bipartite.minimum_weight_full_matching(G, top_nodes=None, weight='weight')
