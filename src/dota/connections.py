9eb51d5ee697f08aff5d96ad1b6bc360fe2a2000
Owleyy <owleyowlington@gmail.com>
2023-08-31 14:59:39 +0100

    reverting back

--- src/dota/connections.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-def getCredentialsSteam():
-    with open('credentials/credentialsSteam.yml') as f:
-        data = yaml.safe_load(f)
-        return (data['USERNAME'], data['PASSWORD'])
