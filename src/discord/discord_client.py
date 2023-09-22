fe00167f3c247a87b13ab706b7ab5ed420cba27a
Samscranch <140660422+Samscranch@users.noreply.github.com>
2023-09-22 16:36:11 +0100

    created a method to check user without precise name

--- src/discord/discord_client.py
+++ src/discord/discord_client.py
@@ -46,36 +46,55 @@ def run_discord_bot():
     @bot.command()
     async def vk(ctx, user):
         chat_channel = data_management.load_config_data(ctx.guild, 'CHANNELS', 'chat_channel')
-        if ctx.channel == discord.utils.get(ctx.guild.channels, id=chat_channel):
-            user_check = check_user.user_exists(ctx.guild, user)
-            chosen_server = next((x for x in server_list if x.server == ctx.guild))
-            if not chosen_server:
-                await ctx.send(content=f'Commands are not yet working, please ensure setup has been completed and the bot has been restarted',
-                               ephemeral=True)
-            elif not user_check[0] or user_check[1] not in chosen_server.queued_players:
-                await ctx.send(content=f'{user} isn\'t in the queue', ephemeral=True)
-            else:
-                await chosen_server.vote_kick(ctx.guild, user_check[1], ctx.author, ctx.channel)
+        if ctx.channel != discord.utils.get(ctx.guild.channels, id=chat_channel):
+            return
+        user_id = user[2:-1]
+        user_acc = await ctx.guild.fetch_member(user_id)
+        chosen_server = next((x for x in server_list if x.server == ctx.guild))
+        if not chosen_server:
+            await ctx.send(content=f'Commands are not yet working, please ensure setup has been completed and the bot has been restarted',
+                           ephemeral=True)
+        elif ctx.message.author not in chosen_server.queued_players:
+            await ctx.send(content=f'You aren\'t in the queue!', ephemeral=True)
+        elif len(chosen_server.queued_players) < 10:
+            await ctx.send(content=f'Votekick can only be used when the queue is full', ephemeral=True)
+        elif user_acc not in chosen_server.queued_players:
+            await ctx.send(content=f'{user} isn\'t in the queue', ephemeral=True)
+        else:
+            await chosen_server.vote_kick(ctx.guild, user_acc, ctx.author, ctx.channel)
 
     @bot.command()
     async def wh(ctx, user):
         chat_channel = data_management.load_config_data(ctx.guild, 'CHANNELS', 'chat_channel')
-        if ctx.channel == discord.utils.get(ctx.guild.channels, id=chat_channel):
-            user_check = check_user.user_exists(ctx.guild, user)
-            chosen_server = next((x for x in server_list if x.server == ctx.guild))
-            if not chosen_server:
-                await ctx.send(
-                    content=f'Commands are not yet working, please ensure setup has been completed and the bot has been restarted',
-                    ephemeral=True)
-            elif not user_check[0]:
-                await ctx.send(content=f'{user} not found', ephemeral=True)
-            else:
-                user_data = data_management.view_user_data(user_check[1].id)
-                await ctx.send(embed=check_user.user_embed(user_data, user_check[1], ctx.guild))
+        if ctx.channel != discord.utils.get(ctx.guild.channels, id=chat_channel):
+            return
+        user_id = user[2:-1]
+        user_acc = await ctx.guild.fetch_member(user_id)
+        user_check = check_user.registered_check(int(user_id))
+        chosen_server = next((x for x in server_list if x.server == ctx.guild))
+        if not chosen_server:
+            await ctx.send(
+                content=f'Commands are not yet working, please ensure setup has been completed and the bot has been restarted',
+                ephemeral=True)
+        elif not user_check:
+            await ctx.send(content=f'{user_acc.display_name} not found', ephemeral=True)
+        else:
+            user_data = data_management.view_user_data(user_id)
+            await ctx.send(embed=check_user.user_embed(user_data, user_acc, ctx.guild))
 
-    # @bot.command()
-    # async def testing(ctx):
-    #     await ctx.send(f"{test}")
+    @bot.command()
+    async def register(ctx, dotabuff_id, mmr):
+        print(dotabuff_id)
+        print(mmr)
+        chat_channel = data_management.load_config_data(ctx.guild, 'CHANNELS', 'chat_channel')
+        registered_role_id = data_management.load_config_data(ctx.guild, 'ROLES', 'registered_role')
+        if ctx.channel != discord.utils.get(ctx.guild.channels, id=chat_channel):
+            return
+
+    @bot.command()
+    async def testing(ctx, chosen_string):
+        user = next((x for x in ctx.guild.members if chosen_string.lower() in x.display_name.lower()))
+        await ctx.send(user.display_name)
 
     @bot.command()
     # Used to post the help button, currently not being worked on (name to be amended)
