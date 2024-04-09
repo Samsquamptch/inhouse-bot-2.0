1e10f430844e9387fcde3eaa20fe2adf8219a8f2
Samscranch <140660422+Samscranch@users.noreply.github.com>
2024-04-09 01:25:08 +0100

    Lobbies are now created and start once both teams have 5 players

--- src/dota/dota_client.py
+++ src/dota/dota_client.py
@@ -1,6 +1,7 @@
 from steam.client import SteamClient, SteamID
 from dota2.client import Dota2Client
 import multiprocessing
+from time import sleep
 import dota2
 import os
 import sys
@@ -33,93 +34,95 @@ Manager = dota2.features.chat.ChannelManager(dota, 'logger')
 def start_dota():
     dota.launch()
     print("Running...")
- 
-
-@dota.on('ready')
-def fetch_profile_card():
-    dota.request_profile_card(70388657)
 
 @client.on('disconnected')
 def kill_lobby():
     dota.destroy_lobby()
 
 @dota.on('dota_welcome')
-def create_base_lobby():
-
+def delete_lobby(filler):
     dota.destroy_lobby()
+    print("!")
+    sleep(5)
+    dota.emit('make_lobby')
+
+@dota.on('make_lobby')
+def create_new_lobby():
     opt = {
-            'game_name': 'Nest Test Lobby2',
-            'game_mode': dota2.enums.DOTA_GameMode.DOTA_GAMEMODE_CM,
-            'fill_with_bots': False,
-            'allow_spectating': True,
-            'allow_cheats': False,
-            'allchat': False,
-            'dota_tv_delay': 0,  # TODO: this is LobbyDotaTV_10
-            'pause_setting': 0,  # TODO: LobbyDotaPauseSetting_Unlimited
-        }
-    dota.create_practice_lobby(password="1", options=opt)
+        'game_name': 'DH Test Lobby',
+        'server_region': dota2.enums.EServerRegion.Europe,
+        'game_mode': dota2.enums.DOTA_GameMode.DOTA_GAMEMODE_CM,
+        'leagueid' : 14994,
+        'fill_with_bots': False,
+        'allow_spectating': True,
+        'allow_cheats': False,
+        'allchat': False,
+        'dota_tv_delay': 0,
+        'pause_setting': 0
+    }
+    dota.create_practice_lobby(password="woof", options=opt)
     dota.channels.join_lobby_channel()
     Manager.join_lobby_channel()
-    dota.join_practice_lobby_team()  # jump to unassigned players
-    event.emit('check_queue')
+    # dota.join_practice_lobby_broadcast_channel(channel=1)
+    # dota.join_practice_lobby()
+    # dota.join_practice_lobby_team()  # jump to unassigned players
 
-@dota.on(dota2.features.Lobby.EVENT_LOBBY_NEW) 
+@dota.on(dota2.features.Lobby.EVENT_LOBBY_NEW)
 def do_dota_wait(lobby):
     dota.emit('start_queue', lobby)
 
 
-@dota.on('starting_queue')
-def do_queue(lobby, idList):
-    dota.emit('queue_full', idList)
-    print('Queue Full...')
+# @dota.on('starting_queue')
+# def do_queue(lobby, idList):
+#     dota.emit('queue_full', idList)
+#     print('Queue Full...')
 
-@dota.on('queue_full')
-def dota_invite(ids):    
-    print('Inviting...')
-    for i in ids:
-        print(SteamID(i))
-        print(f'inviting {i}')
-        print(i+76561197960265728)
-        dota.invite_to_lobby(i+76561197960265728)
+# @dota.on('queue_full')
+# def dota_invite(ids):
+#     print('Inviting...')
+#     for i in ids:
+#         print(SteamID(i))
+#         print(f'inviting {i}')
+#         print(i+76561197960265728)
+#         dota.invite_to_lobby(i+76561197960265728)
 
-@dota.on('lobby_changed')
-def change_lobby(lobby):
-    pass
-
-@event.on('check_queue')
-def check_queue():
-    if os.path.isfile('data/activate.txt'):
-        dota.join_practice_lobby_team() 
-        df = pd.DataFrame(pd.read_csv('data/match.csv'))
-        ids = list(df['steam']) 
-        stat.set_game(ids)
-        os.remove('data/activate.txt')
-        dota.emit('queue_full', ids)
-    time.sleep(0.1)
-    event.emit('check_queue')
-
+# @dota.on('lobby_changed')
+# def change_lobby(lobby):
+#     pass
+#
+# @event.on('check_queue')
+# def check_queue():
+#     if os.path.isfile('data/activate.txt'):
+#         dota.join_practice_lobby_team()
+#         df = pd.DataFrame(pd.read_csv('data/match.csv'))
+#         ids = list(df['steam'])
+#         stat.set_game(ids)
+#         os.remove('data/activate.txt')
+#         dota.emit('queue_full', ids)
+#     time.sleep(0.1)
+#     event.emit('check_queue')
+#
 @Manager.on('message')
 def message_check(c, message):
+    print("message!")
     if message.text.startswith('!'):
         if message.text == '!start':
+            print("test2")
             players = []
             users = dota.lobby.all_members
             for user in users:
                 if user.team == 1 or user.team == 0:
                     players.append(user)
-            if players == []:
+            if not players:
                 print('No Players')
                 return False
-            for player in players:
-                print(SteamID(player.id).as_32)
-                print(stat.get_game())
-                if SteamID(player.id).as_32 not in stat.get_game():
-                    return False
-            if len(players) == 10:
+            elif len(players) == 10:
                 dota.launch_practice_lobby()
-
-def check_users():
-    pass
+            else:
+                print("Number of players is only " + str(len(players)))
+# #
+# def check_users():
+#     pass
 
 client.cli_login(username=user, password=password)
 client.run_forever()
